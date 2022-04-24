3cb56b1470e5e31ff49f09f1785b79f2602418d2
Grey Larrain Bratt <greybratt@outlook.com>
2022-04-23 17:57:09 -0700

    first attempt at async function

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 // the format is: fetch().then().then().catch()
 //it's easier to read if we put each step in its own line,
 //that's why the periods start the then lines.
-
 fetch("houses.json")
     .then((response) => response.json())
     .then((data) => {
@@ -44,6 +43,41 @@ fetch("https://x-colors.herokuapp.com/api/random?number=2")
         let color2 = data[1].rgb;
         docCss.backgroundColor = color1;
         containerCss.backgroundColor = color2;
-        containerCss.filter = "brightness(125%)";
+        containerCss.filter = "brightness(125%) saturation(50%)";
     })
     .catch(err => console.log("Color error!", err));
+
+async function useData() {
+    await retrieveData();
+    then((response) => response.json())
+    .then((data) => {
+        //create a temp holder to append all the html generated inside the forEach iterator
+        let html = "";
+
+        //the argument "house" passed to the arrow function
+        //holds each item in the array in turn.fetch("houses.json")
+        data.forEach(house => {
+            let family = house.members.join(", ");
+
+            // generate the html snippet for one array item
+            //to be added to the "html" temp holder.
+            let objInfo =
+            `<dl>
+                <dt class="house">${house.name}</dt>
+                <dd class="people">${family}</dd>
+            </dl>`;
+            html += objInfo;
+        });
+
+        //make a reference to the html container where
+        //the info will be displayed.
+        const container = document.querySelector("#container");
+        container.innerHTML = html;
+    })
+    .catch(err => console.log("GoT error!", err));
+    //this only runs if there is an error during the above process
+};
+
+function retrieveData() {
+    new Promise((resolve) => fetch("houses.json"), resolve("done"));
+}
